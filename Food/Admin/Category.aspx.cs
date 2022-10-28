6e3e42243093262bf49606fc483d515f1cc95b23
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-10-28 23:31:01 +0300

    adding connection.cs

--- Food/Admin/Category.aspx.cs
+++ Food/Admin/Category.aspx.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace Food.Admin
 {
+    
     public partial class Category : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
---
+++ Food/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Food
+{
+    public class Connection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+        
+    }
+}
