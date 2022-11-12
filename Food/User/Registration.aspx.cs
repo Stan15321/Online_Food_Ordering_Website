48cb34add2b5442496d469adec5dd3f028d8a629
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-11-12 13:42:06 +0200

    updating registration.aspx

--- Food/User/Registration.aspx.cs
+++ Food/User/Registration.aspx.cs
@@ -18,7 +18,17 @@ namespace Food.User
         DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["id"] != null) //&& Session["userId"] !=null
+                {
+                    GetUserDetails();
+                }
+                else if (Session["userId"] != null)
+                {
+                    Response.Redirect("Default.aspx");
+                }
+            }
         }
 
         protected void btnRegister_Click(object sender, EventArgs e)
@@ -101,6 +111,37 @@ namespace Food.User
                     con.Close();
                 }
             }
+        }
+
+        void GetUserDetails()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("User_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "SELECT4PROFILE");
+            cmd.Parameters.AddWithValue("@UserId", Request.QueryString["id"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows.Count == 1)
+            {
+                txtName.Text = dt.Rows[0]["Name"].ToString();
+                txtUsername.Text = dt.Rows[0]["Username"].ToString();
+                txtMobile.Text = dt.Rows[0]["Mobile"].ToString();
+                txtEmail.Text = dt.Rows[0]["Email"].ToString();
+                txtAddress.Text = dt.Rows[0]["Address"].ToString();
+                txtPostCode.Text = dt.Rows[0]["PostCode"].ToString();
+                imgUser.ImageUrl = string.IsNullOrEmpty(dt.Rows[0]["ImageUrl"].ToString())
+                    ? "../Images/No-image.png" : "../" + dt.Rows[0]["ImageUrl"].ToString();
+                imgUser.Height = 200;
+                imgUser.Width = 200;
+                txtPassword.TextMode = TextBoxMode.SingleLine;
+                txtPassword.ReadOnly = true;
+                txtPassword.Text = dt.Rows[0]["Password"].ToString();
+            }
+            lblHeaderMsg.Text = "<h2>Edit Profile</h2>";
+            btnRegister.Text = "Update";
+            lblAlreadyUser.Text = "";
         }
 
         private void Clear()
