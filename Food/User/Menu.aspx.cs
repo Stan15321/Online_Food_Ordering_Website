655f9b86cb4160a07bb042e9683ae0614da593aa
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-11-22 22:15:24 +0200

    adding one procedure and updating menu.aspx.cs

--- Food/User/Menu.aspx.cs
+++ Food/User/Menu.aspx.cs
@@ -20,22 +20,23 @@ namespace Food.User
         {
             if (!IsPostBack)
             {
+                GetCategories();
                 GetProducts();
             }
         }
-        //private void GetCategories()
-        //{
-        //    con = new SqlConnection(Connection.GetConnectionString());
-        //    cmd = new SqlCommand("Category_Crud", con);
-        //    cmd.Parameters.AddWithValue("@Action", "SELECT");
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    sda = new SqlDataAdapter(cmd);
-        //    dt = new DataTable();
-        //    sda.Fill(dt);
-        //    rP.DataSource = dt;
-        //    rCategory.DataBind();
+        private void GetCategories()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "ACTIVECAT");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rCategory.DataSource = dt;
+            rCategory.DataBind();
 
-        //}
+        }
         private void GetProducts()
         {
             con = new SqlConnection(Connection.GetConnectionString());
