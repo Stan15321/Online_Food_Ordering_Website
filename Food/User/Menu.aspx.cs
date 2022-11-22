b552f60ac229acd2fcd4c52e2cd704d95f7c64c7
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-11-22 21:53:24 +0200

    making a procedure for ACTIVEPROD and editing Menu.aspx.cs

--- Food/User/Menu.aspx.cs
+++ Food/User/Menu.aspx.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Food.Admin;
 
 namespace Food.User
 {
     public partial class Menu : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetProducts();
+            }
+        }
+        //private void GetCategories()
+        //{
+        //    con = new SqlConnection(Connection.GetConnectionString());
+        //    cmd = new SqlCommand("Category_Crud", con);
+        //    cmd.Parameters.AddWithValue("@Action", "SELECT");
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    sda = new SqlDataAdapter(cmd);
+        //    dt = new DataTable();
+        //    sda.Fill(dt);
+        //    rP.DataSource = dt;
+        //    rCategory.DataBind();
+
+        //}
+        private void GetProducts()
+        {
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Product_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "ACTIVEPROD");
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            rProducts.DataSource = dt;
+            rProducts.DataBind();
 
         }
     }
